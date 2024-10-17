8035ab558dacdf60b4c3c8e2f427ef6fc90129fc
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-17 13:12:52 -0300

    atip.cust

--- src/app/pages/log/customuseratip/customuseratip.component.ts
+++ src/app/pages/log/customuseratip/customuseratip.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './customuseratip.component.css'
 })
 export class CustomuseratipComponent {
+  sobrecarregado: number | null = null;
+  verbal: number | null = null;
+  contatoFisico: number | null = null;
 
+  mudarCor(pergunta: number, valor: number) {
+    if (pergunta === 0) {
+      this.sobrecarregado = this.sobrecarregado === valor ? null : valor;
+    } else if (pergunta === 1) {
+      this.verbal = this.verbal === valor ? null : valor;
+    } else if (pergunta === 2) {
+      this.contatoFisico = this.contatoFisico === valor ? null : valor;
+    }
+  }
 }
--- src/app/pages/log/customuserprof/customuserprof.component.ts
+++ src/app/pages/log/customuserprof/customuserprof.component.ts
@@ -6,20 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './customuserprof.component.css'
 })
 export class CustomuserprofComponent {
-  sobrecarregado: number | null = null;
-  verbal: number | null = null;
-  contatoFisico: number | null = null;
-  semi: number | null = null;
-
-  mudarCor(pergunta: number, valor: number) {
-    if (pergunta === 0) {
-      this.sobrecarregado = this.sobrecarregado === valor ? null : valor;
-    } else if (pergunta === 1) {
-      this.verbal = this.verbal === valor ? null : valor;
-    } else if (pergunta === 2) {
-      this.contatoFisico = this.contatoFisico === valor ? null : valor;
-    } else if (pergunta === 3) {
-      this.semi = this.semi === valor ? null : valor;
-    }
-  }
+  
 }
