ec359e89e4674bc4c1bf09fe3712e0ade5061b4e
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-24 13:35:09 -0300

    med e config pages

--- src/app/pages/atipico/crisissettings/crisissettings.component.ts
+++ src/app/pages/atipico/crisissettings/crisissettings.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './crisissettings.component.css'
 })
 export class CrisissettingsComponent {
+  
+  sobrecarregado: number | null = null;
+  verbal: number | null = null;
+  contatoFisico: number | null = null;
 
+  mudarCor(pergunta: number, valor: number) {
+    if (pergunta === 0) {
+      this.sobrecarregado = this.sobrecarregado === valor ? null : valor;
+    } else if (pergunta === 1) {
+      this.verbal = this.verbal === valor ? null : valor;
+    } else if (pergunta === 2) {
+      this.contatoFisico = this.contatoFisico === valor ? null : valor;
+    }
+  }
 }
--- src/app/pages/tipico/custom/id/medication/medication.component.ts
+++ src/app/pages/tipico/custom/id/medication/medication.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './medication.component.css'
 })
 export class MedicationComponent {
+  horarios: { hora: string, minuto: string, periodo: string }[] = [
+    { hora: '', minuto: '', periodo: 'AM' }
+  ];
 
+  alternarAmPm(index: number) {
+    this.horarios[index].periodo = this.horarios[index].periodo === 'AM' ? 'PM' : 'AM';
+  }
+
+  adicionarHorario() {
+    this.horarios.push({ hora: '', minuto: '', periodo: 'AM' });
+  }
+
+  removerHorario(index: number) {
+    this.horarios.splice(index, 1);
+  }
 }
